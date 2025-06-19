0cfcf12794ff7cfc20d110831eb94c52d9f19519
Nigel Horne <njh@bandsman.co.uk>
2025-06-19 13:12:05 -0400

    Get module versions live

--- lib/VersionCheck.pm
+++ lib/VersionCheck.pm
@@ -2,6 +2,8 @@ package VersionCheck;
 use Exporter 'import';
 our @EXPORT_OK = qw(check_module_versions load_allowed_versions);
 
+use MetaCPAN::Client;
+
 sub load_allowed_versions {
     my $file = shift;
     open my $fh, '<', $file or die "Can't open allowlist $file: $!";
@@ -16,13 +18,26 @@ sub load_allowed_versions {
 
 sub check_module_versions {
     my ($line, $file, $line_no, $ref, $allowed_ref) = @_;
+    my $cpan = MetaCPAN::Client->new();
+
     if ($line =~ /^\s*use\s+([\w:]+)\s*(\d+(\.\d+)*)?/) {
         my ($module, $ver) = ($1, $2 || '0');
+
         if (!exists $allowed_ref->{$module}) {
-            push @$ref, [$file, $line_no, "Unknown module '$module'"];
+            push @$ref, [$file, $line_no, "Unknown module '$module' used"];
         } elsif ($ver < $allowed_ref->{$module}) {
             push @$ref, [$file, $line_no, "Outdated '$module' version $ver (expected $allowed_ref->{$module})"];
         }
+
+        eval {
+            my $release = $cpan->release($module);
+            my $latest  = $release->version;
+            if ($latest && $ver < $latest) {
+                push @$ref, [$file, $line_no, "Live CPAN check: '$module' version $ver is older than latest $latest"];
+            }
+        };
+        warn "MetaCPAN error: $@" if $@;
     }
 }
+
 1;
